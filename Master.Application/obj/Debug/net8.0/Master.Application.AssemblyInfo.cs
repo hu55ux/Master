240eecf9bc15f19c1dfe987df130b45191ab3e38
Huseyn <hsbziyev@gmail.com>
2026-03-20 14:28:00 +0400

    Pipe

--- Master.Application/obj/Debug/net8.0/Master.Application.AssemblyInfo.cs
+++ Master.Application/obj/Debug/net8.0/Master.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70fed48aa45d6b5ec27f95aebee463fd68a7fb8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae24250201c6198ac24b40f1dd145f7fe6fa341e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
